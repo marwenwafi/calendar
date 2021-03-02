b8a6d0a78b95243f59909e514e85bc16a1ebaac4
marwenwafi <marwen.wafi@esprit.tn>
2021-03-02 22:03:11 +0100

    just after scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConsultationsComponent } from './consultations/consultations.component';
 import { HttpClientModule } from '@angular/common/http';
+import { AngularMyDatePickerModule } from 'angular-mydatepicker';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    AngularMyDatePickerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/consultations/consultations.component.ts
+++ src/app/consultations/consultations.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
 import {ConsulationService} from "../services/consulation.service";
+import {IMyDpOptions} from 'mydatepicker';
+import { ViewChildren, QueryList } from '@angular/core';
+
 
 @Component({
   selector: 'app-consultations',
   templateUrl: './consultations.component.html',
-  styleUrls: ['./consultations.component.css']
+  styleUrls: ['./consultations.component.scss']
 })
 export class ConsultationsComponent implements OnInit {
 
   consultationslist;
 
   constructor(private consService: ConsulationService) { }
+
+  @ViewChildren('cmp') components:ElementRef;
+
+  ngAfterViewInit(){
+    // print array of CustomComponent objects
+    console.log(this.components);
+  }
 
   ngOnInit(): void {
     this.consService.getAllConsultations().subscribe(
